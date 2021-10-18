b5b1208066dba26e68ae19b93fe078ab1b8d8376
haolingyi <1031909300@qq.com>
2021-10-18 22:01:57 +0800

    test

---
+++ GO/demo1.go
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2021-10-18 21:57:04
+ * @LastEditTime: 2021-10-18 21:58:23
+ * @LastEditors: Lingyihao
+ * @Description:
+ */
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello,World!")
+}
